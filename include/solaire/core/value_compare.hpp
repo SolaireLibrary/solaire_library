df910a9201fce17cf099ec33f21baeedbf758dd5
SolaireLibrary <solairelibrary@mail.com>
2016-09-23 09:06:44 +0100

    Added documentation for value_compare

--- include/solaire/core/value_compare.hpp
+++ include/solaire/core/value_compare.hpp
@@ -18,20 +18,28 @@
 #include "core.hpp"
 
 namespace solaire {
-	template<class T, const T A, const T B, typename ENABLE = void>
+
+	/*!
+		\param Compare two values.
+		\tparam T The type of the values being compared.
+		\tparam A The first value.
+		\tparam B The second value.
+		\version 2.0
+	*/
+	template<class T, const T A, const T B>
 	struct value_compare {
 		enum {
-			LESS_THAN = A < B,
-			GREATER_THAN = A > B,
-			EQUAL = A == B,
-			NOT_EQUAL = A != B,
-			LESS_THAN_EQUAL = LESS_THAN || EQUAL,
-			GREATER_THAN_EQUAL = GREATER_THAN || EQUAL
+			LESS_THAN = A < B,							//!< 1 If A < B else 0.
+			GREATER_THAN = A > B,						//!< 1 If A > B else 0.
+			EQUAL = A == B,								//!< 1 If A == B else 0.
+			NOT_EQUAL = A != B,							//!< 1 If A != B else 0.
+			LESS_THAN_EQUAL = LESS_THAN || EQUAL,		//!< 1 If A <= B else 0.
+			GREATER_THAN_EQUAL = GREATER_THAN || EQUAL	//!< 1 If A >= B else 0.
 		};
-		static constexpr T LOW_VALUE = A < B ? A : B;
-		static constexpr T HIGH_VALUE = A < B ? B : A;
-		static constexpr T FIRST = A;
-		static constexpr T SECOND = B;
+		static constexpr T LOW_VALUE = A < B ? A : B;	//!< Thow lesser of A and B.
+		static constexpr T HIGH_VALUE = A < B ? B : A;	//!< Thow greater of A and B.
+		static constexpr T FIRST = A;					//!< A.
+		static constexpr T SECOND = B;					//!< B.
 	};
 }
 
