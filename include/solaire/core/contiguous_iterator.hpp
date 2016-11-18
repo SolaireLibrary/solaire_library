f8fcf0112f2d44ddd35dc8642e6b5bc4217d1665
SolaireLibrary <solairelibrary@mail.com>
2016-11-18 16:12:27 +0000

    New iterator class

--- include/solaire/core/contiguous_iterator.hpp
+++ include/solaire/core/contiguous_iterator.hpp
@@ -19,22 +19,50 @@
 
 namespace solaire {
 	template<class T>
-	class contiguous_iterator_template {
-	private:
-		T* mPtr;
-	public:
-		typedef T type;
-		contiguous_iterator_template() : mPtr(nullptr) {}
-		contiguous_iterator_template(T* const aPtr) : mPtr(aPtr) {}
-		inline void forward(const int aOffset) { mPtr += aOffset; }
-		inline void backward(const int aOffset) { mPtr -= aOffset };
-		inline int get_offset(const contiguous_iterator_template<T>& aOther) const { return mPtr - aOther.mPtr; }
-		inline T* get() { return *mPtr; }
-	};
+	const iterator_helper& create_contiguous_iterator(T* aBase, void* aData) {
+		struct data {
+			T* ptr;
+			int32_t offset;
+		};
 
-	template<class T>
-	using contiguous_iterator = iterator<contiguous_iterator_template<T>, false>;
+		static iterator_helper OBJ = {
+			[](void* aObj)->void* {
+			data& d = *static_cast<data*>(aObj);
+			return d.ptr + d.offset;
+		},
+			[](void* aObj, int32_t aOffset)->void {
+			data& d = *static_cast<data*>(aObj);
+			d.offset += aOffset;
+		},
+			[](const void* aObj, const void* aOther)->int32_t {
+			const data& a = *static_cast<const data*>(aObj);
+			const data& b = *static_cast<const data*>(aOther);
+			return a.offset - b.offset;
+		}
+		};
+		data& d = *static_cast<data*>(aData);
+		d.ptr = aBase;
+		d.offset = 0;
+		return OBJ;
+	}
 
-	template<class T>
-	using const_contiguous_iterator = iterator<contiguous_iterator_template<T>, true>;
+	//template<class T>
+	//class contiguous_iterator_template {
+	//private:
+	//	T* mPtr;
+	//public:
+	//	typedef T type;
+	//	contiguous_iterator_template() : mPtr(nullptr) {}
+	//	contiguous_iterator_template(T* const aPtr) : mPtr(aPtr) {}
+	//	inline void forward(const int aOffset) { mPtr += aOffset; }
+	//	inline void backward(const int aOffset) { mPtr -= aOffset };
+	//	inline int get_offset(const contiguous_iterator_template<T>& aOther) const { return mPtr - aOther.mPtr; }
+	//	inline T* get() { return *mPtr; }
+	//};
+
+	//template<class T>
+	//using contiguous_iterator = iterator<contiguous_iterator_template<T>, false>;
+
+	//template<class T>
+	//using const_contiguous_iterator = iterator<contiguous_iterator_template<T>, true>;
 }
